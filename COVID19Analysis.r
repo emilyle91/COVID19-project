929880de214a56732ae32552fd0e032f67b5914d
Emily Le <131802939+emilyle91@users.noreply.github.com>
2023-04-30 16:41:49 +0700

    finish age and gender hypothesis testing and conclusion

--- COVID19Analysis.r
+++ COVID19Analysis.r
@@ -28,10 +28,6 @@ data$recovered_dummy <- as.integer(data$recovered != 0)
 death_rate <- sum(data$death_dummy)/nrow(data)
 print(death_rate)
 
-#calculate the recovered rate 
-recovered_rate <- sum(data$recovered_dummy)/nrow(data)
-print(recovered_rate)
-
 #Hypothesis 1: People died from COVID older than people who survived
 #Test the hypothesis 
 
@@ -46,3 +42,22 @@ mean(alive$age, na.rm = TRUE)
 #Conduct the statistical differences
 t.test(dead$age, alive$age, alternative = "two.sided", conf.level = 0.95)
 
+#P-value = 0 < 0.05 --> the mean is significant differences
+#At 95% CI, the dead age's mean is 16.7 to 24.3 years more than the the alive age's mean
+#Conclusion: reject null hypothesis, finding that the people die from COVID is older.
+
+#Hypothesis 2: Female have lower death rate than male
+#Create two required data subset: Women and men
+women =  subset(data, data$gender == "female")
+men = subset(data, data$gender == "male")
+
+#Compare the death rate between two gender (another mean code)
+sum(women$death_dummy)/nrow(women) #death rate of women = 3.6%
+sum(men$death_dummy)/nrow(men) #death rate of men = 8.46%
+
+#Conduct statistical importance
+t.test(women$death_dummy, men$death_dummy, alternative = "two.sided", conf.level = 0.95)
+
+#P-value = 0.002 < 0.05 --> the mean is significant difference
+#At 95% CI, women death rate is lower from 1.7% to 7.8% than men death rate
+#Conclusion: reject the null hypothesis (female have the same death rate with male), finding that female have lower death rate than male
